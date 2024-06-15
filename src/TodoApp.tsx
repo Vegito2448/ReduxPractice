1b44144027c2a0e371d8a196fd75b9ac289d2be7
Jesús Peña <jesusdpeaj18102001@gmail.com>
2024-06-14 22:34:14 -0400

    RTK practice Query endded

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,19 +1,47 @@
-import { useGetTodosQuery } from "./store";
+import { useState } from "react";
+import { Todo, useGetTodoQuery, useGetTodosQuery } from "./store";
 
 const TodoApp = () => {
-  const { ...data } = useGetTodosQuery(undefined);
 
-  console.log(`🚀 ~ TodoApp ~ data:`, data);
+  const [todoId, setTodoId] = useState(1);
+
+  const { data: todos, isLoading } = useGetTodosQuery(undefined);
+
+  const { data: todo, isLoading: isTodoLoading } = useGetTodoQuery(todoId);
+
+  console.log(`🚀 ~ TodoApp ~ todo:`, todo);
+
+  console.log(`🚀 ~ TodoApp ~ data:`, todos);
+
+
+  const increaseBy = (increaseBy: number) => setTodoId(prevTodo => Math.max(1, prevTodo + increaseBy));
 
   return (
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
-      <span>Loading...</span>
-      <pre>...</pre>
-      <button /* aria-disabled={true} disabled={true} */>
+      {isLoading && <span>Loading...</span>}
+      {!isTodoLoading && <pre>{JSON.stringify(todo)}</pre>}
+
+
+      <button
+        onClick={() => increaseBy(-1)}
+      >
+        Prev Todo
+      </button>
+      <button
+        onClick={() => increaseBy(1)}
+      >
         Next
       </button>
+      <ul>
+        {!isLoading && todos?.map((todo: Todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.completed} />
+            <span>{todo.title}</span>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
--- src/store/api/todosApi.ts
+++ src/store/api/todosApi.ts
@@ -1,24 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+export interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+
 export const todosApi = createApi({
   reducerPath: 'todos',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
-  tagTypes: ['Todo'],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => '/todos',
-      providesTags: ['Todo'],
     }),
-    addTodo: builder.mutation({
-      query: (body) => ({
-        url: '/todos',
-        method: 'POST',
-        body,
-      }),
-      invalidatesTags: ['Todo'],
+    getTodo: builder.query({
+      query: (todoId) => `/todos/${todoId}`,
     }),
   }),
 });
 
 
-export const { useGetTodosQuery, useAddTodoMutation } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
